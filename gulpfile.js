94e76ed099d152d95986bbca0b14a72fc0a283f9
jyackley <jyackley@msts.com>
2018-02-12 20:12:40 -0600

    Added a task to debug and a default.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,24 @@ const gulp = require('gulp');
 const nodemon = require('nodemon');
 const jsFiles = ['*.js', 'server/**/*.js'];
 
+gulp.task('debug', ()=>{
+    let options = {
+      script: 'app.js',
+      delayTime: 1,
+      env: {
+        'PORT':3000
+      },
+      watch: jsFiles,
+      execMap: {
+        js: "node --inspect"
+      }
+    };
+    return nodemon(options)
+      .on('restart', ()=>{
+        console.log('restarting server...');
+      });
+});
+
 gulp.task('serve', ()=>{
   let options = {
     script: 'app.js',
@@ -16,3 +34,5 @@ gulp.task('serve', ()=>{
     console.log('restarting server...');
   });
 });
+
+gulp.task('default', ['serve']);
